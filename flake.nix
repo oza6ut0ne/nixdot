63ddbd4a5266db3fce5892ab72ca5a364649efc6
oza6ut0ne <33759728+oza6ut0ne@users.noreply.github.com>
2025-02-08 23:33:17 +0900

    Migrate to nixfmt-rfc-style

--- flake.nix
+++ flake.nix
@@ -6,19 +6,34 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{ ... }:
+  outputs =
+    inputs@{ ... }:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [ ];
 
-      systems =
-        [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
 
-      perSystem = { config, self', inputs', pkgs, lib, system, ... }: {
-        packages = lib.packagesFromDirectoryRecursive {
-          inherit (pkgs) callPackage;
-          directory = ./pkgs;
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          lib,
+          system,
+          ...
+        }:
+        {
+          packages = lib.packagesFromDirectoryRecursive {
+            inherit (pkgs) callPackage;
+            directory = ./pkgs;
+          };
         };
-      };
 
       flake = { };
     };
--- pkgs/hts-engine/package.nix
+++ pkgs/hts-engine/package.nix
@@ -3,8 +3,7 @@ stdenv.mkDerivation {
   pname = "hts_engine";
   version = "1.10";
   src = builtins.fetchTarball {
-    url =
-      "https://downloads.sourceforge.net/hts-engine/hts_engine_API-1.10.tar.gz";
+    url = "https://downloads.sourceforge.net/hts-engine/hts_engine_API-1.10.tar.gz";
     sha256 = "0hbwr220wiv1nbvdvahl77061qbq3m39mj0x2bla6p5shd4x23m4";
   };
 }
