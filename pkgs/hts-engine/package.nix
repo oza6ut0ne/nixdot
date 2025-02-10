4371b56ae4f828a3f54d37c51fab1c4a8e69c233
oza6ut0ne <33759728+oza6ut0ne@users.noreply.github.com>
2025-02-10 20:30:01 +0900

    Fix build error of HTS Engine on musl

--- pkgs/hts-engine/package.nix
+++ pkgs/hts-engine/package.nix
@@ -6,4 +6,6 @@ stdenv.mkDerivation {
     url = "https://downloads.sourceforge.net/hts-engine/hts_engine_API-1.10.tar.gz";
     sha256 = "0hbwr220wiv1nbvdvahl77061qbq3m39mj0x2bla6p5shd4x23m4";
   };
+
+  patches = if stdenv.hostPlatform.isMusl then [ ./fix-for-musl.patch ] else [ ];
 }
