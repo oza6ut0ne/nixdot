7cbd92e0e7b04b08dba1cd188ffa0cd96574b46a
oza6ut0ne <33759728+oza6ut0ne@users.noreply.github.com>
2025-06-21 11:21:12 +0900

    Add activation script and registries

--- home.nix
+++ home.nix
@@ -35,6 +35,55 @@
 
   };
 
+  home.activation = {
+    linkToHomeManagerConfigDir = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      src_dir="${homeDirectory}/nixdot"
+      dst_dir="${config.xdg.configHome}/home-manager"
+      if [ -d "''${src_dir}" ] && [ ! -d "''${dst_dir}" ]; then
+        run ln $VERBOSE_ARG -sT "''${src_dir}" "''${dst_dir}"
+      fi
+    '';
+  };
+
+  nix.registry = {
+    n = {
+      exact = false;
+      from = {
+        id = "n";
+        type = "indirect";
+      };
+      to = {
+        id = "nixpkgs";
+        type = "indirect";
+      };
+    };
+
+    dot = {
+      exact = false;
+      from = {
+        id = "dot";
+        type = "indirect";
+      };
+      to = {
+        owner = "oza6ut0ne";
+        repo = "nixdot";
+        type = "github";
+      };
+    };
+
+    dotlocal = {
+      exact = false;
+      from = {
+        id = "dotlocal";
+        type = "indirect";
+      };
+      to = {
+        type = "path";
+        path = "${homeDirectory}/nixdot";
+      };
+    };
+  };
+
   home.username = username;
   home.homeDirectory = homeDirectory;
   home.stateVersion = "25.05";
